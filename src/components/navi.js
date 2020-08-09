7564bd2c615272e6549004c271be6792258c87f8
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-08-09 12:17:17 -0500

    NavBar for mobile created and fixed to top

--- src/components/navi.js
+++ src/components/navi.js
@@ -1,20 +1,44 @@
 import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
+import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
 
 const Navi = () => {
     return (
         <>
-        <Navbar bg="dark" variant="dark">
+        <div className="mobileNav">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
             <Navbar.Brand href="/">
             <img
-                alt=""
-                src={require("./img/LogoBad.jpg")}
-                width="50"
-                height="30"
-                className="d-inline-block align-top"
-            /> <span className="smallTitle"> Serving Tallahassee Since 1969</span>
+                    alt=""
+                    src={require("./img/LogoBad.jpg")}
+                    width="50"
+                    height="30"
+                    className="d-inline-block align-top"
+                />
+                <span className="smallTitle">Bills Signs</span>
             </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/services">Services</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+            </Navbar.Collapse>
         </Navbar>
+        </div>
+        <div className="largeNav">
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">
+                <img
+                    alt=""
+                    src={require("./img/LogoBad.jpg")}
+                    width="50"
+                    height="30"
+                    className="d-inline-block align-top"
+                /> <span className="smallTitle"> Serving Tallahassee since 1969</span>
+                </Navbar.Brand>
+            </Navbar>
+        </div>
         </>
     )
 } 
